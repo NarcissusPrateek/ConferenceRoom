501b3efc45f33f466ba6979a3eaeeb3fd94a6276
nineleaps <prateek.patidar@nineleaps.com>
2019-03-29 18:56:51 +0530

    ip address changed

--- app/src/main/java/com/example/conferencerommapp/Helper/Constants.kt
+++ app/src/main/java/com/example/conferencerommapp/Helper/Constants.kt
@@ -16,8 +16,19 @@ class Constants {
          */
         const val EXTRA_INTENT_DATA = "com.example.conferencerommapp.Activity.EXTRA_INTENT_DATA"
 
+        /**
+         * response code for response IsSuccessfull
+         */
         const val OK_RESPONSE = 200
 
+        /**
+         * building id Name for intents
+         */
         const val EXTRA_BUILDING_ID = "com.example.conferencerommapp.Activity.EXTRA_BUILDING_ID"
+
+        /**
+         * ip address for api call
+         */
+        const val IP_ADDRESS = "http://192.168.1.188/CRB/"
     }
 }
--- app/src/main/java/com/example/conferencerommapp/services/Servicebuilder.kt
+++ app/src/main/java/com/example/conferencerommapp/services/Servicebuilder.kt
@@ -1,5 +1,6 @@
 package com.example.globofly.services
 
+import com.example.conferencerommapp.Helper.Constants
 import com.example.conferencerommapp.services.ConferenceService
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -8,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object Servicebuilder  {
-    private const val URL = "http://192.168.1.188/CRB/"
+
 
     private val logger = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
 
@@ -16,7 +17,7 @@ object Servicebuilder  {
 
 
     private val builder: Retrofit.Builder = Retrofit.Builder()
-                                            .baseUrl(URL)
+                                            .baseUrl(Constants.IP_ADDRESS)
                                             .addConverterFactory(GsonConverterFactory.create()).client(okHttp.build())
     private val retrofit: Retrofit = builder.build()
 
